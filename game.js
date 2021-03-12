c2cad72e041a87965a001714e17c47240e84f96f
Thomas <bergamin.tom@gmail.com>
2021-03-12 15:08:47 +0100

    add behavior when pressing button and create function next sequence

--- game.js
+++ game.js
@@ -1,16 +1,41 @@
 
 let button_colors = ["green", "red", "yellow", "blue"];
 
-let game_pattern = []
+let game_pattern = [];
 
+let user_clicked_pattern = [];
+
+
+function play_sound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
 
 function next_sequence() {
     let random_number = Math.floor(Math.random()*4);
+    let random_chosen_color = button_colors[random_number];
+    game_pattern.push(random_chosen_color);
+    $("#" + game_pattern[0]).fadeIn(220).fadeOut(220).fadeIn(220);
+
+    play_sound(random_chosen_color)
+    
     return random_number
-}
+};
 
-let random_chosen_color = button_colors[next_sequence()];
+function animate_press(current_color) {
+    $("#" + current_color).addClass("pressed")
+    setTimeout(function() {
+        $("#" + current_color).removeClass("pressed")
+    }, 100);
+};
 
-game_pattern.push(random_chosen_color)
+next_sequence();
 
-console.log(game_pattern)
+
+$(".btn").click(function() {
+    let user_chosen_color = this.id;
+    user_clicked_pattern.push(user_chosen_color);
+    play_sound(user_chosen_color);
+    animate_press(user_chosen_color);
+});
+
